c4f6be2ad5a238563e49d380e2b5e06987af8dbe
rohitmukherjee <mukherjeerohit93@gmail.com>
2013-10-13 17:30:13 +0800

    Refactored UDP Server with Stop - Wait ARQ

--- udp_ser1.c
+++ udp_ser1.c
@@ -9,26 +9,28 @@ void transmitAndReceivePackets(int sockfd);
 int main(int argc, char *argv[])
 {
 	int sockfd;
-	struct sockaddr_in my_addr;
-
+	struct sockaddr_in myAddress;
+	
 	//create socket
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {			
 		printf("error in socket");
 		exit(EXIT_FAILURE);
 	}
 
-	my_addr.sin_family = AF_INET;
-	my_addr.sin_port = htons(MYUDP_PORT);
-	my_addr.sin_addr.s_addr = INADDR_ANY;
-	bzero(&(my_addr.sin_zero), 8);
+	//Initialize Socket Address
+	myAddress.sin_family = AF_INET;
+	myAddress.sin_port = htons(MYUDP_PORT);
+	myAddress.sin_addr.s_addr = INADDR_ANY;
+	bzero(&(myAddress.sin_zero), 8);
+
 	//Bind Socket
-	if (bind(sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr)) == -1) {           
+	if (bind(sockfd, (struct sockaddr *) &myAddress, sizeof(struct sockaddr)) == -1) {           
 		printf("error in binding");
 		exit(EXIT_FAILURE);
 	}
 	printf("start receiving\n");
 	while(1) {
-		transmitAndReceivePackets(sockfd);                        // send and receive
+		transmitAndReceivePackets(sockfd);                        
 	}
 	close(sockfd);
 	exit(EXIT_SUCCESS);
@@ -37,30 +39,36 @@ int main(int argc, char *argv[])
 void transmitAndReceivePackets(int sockfd)
 {
 	char receivedMessage[MAXSIZE];
-	int n = 0, len;
-	struct sockaddr_in addr;
+	int n = 0, clientAddresslength = 0;
+	struct sockaddr_in clientAddress;
 	int datagramCount = 0;	
-	len = sizeof (struct sockaddr_in);
+	clientAddresslength = sizeof (struct sockaddr_in);
 	//Initializing acknowledgement
 	struct ack_so acknowledgement;
 	acknowledgement.num = 1;
 	acknowledgement.len = 0;
-
+	
+	
 	//receive the packet
-	if ((n=recvfrom(sockfd, &receivedMessage, DATALEN, 0, (struct sockaddr *)&addr, &len)) == 0) {      
+	if ((n=recvfrom(sockfd, &receivedMessage, DATALEN, 0, (struct sockaddr *)&clientAddress, &clientAddresslength)) > 0) {      
 		datagramCount++;
 		printf("Received Datagram %d \n",datagramCount);
+		printf("the received Datagram is :%s\n", receivedMessage);	
+
 		//Send acknowledgement on receiving the datagram (Stop & Wait Protocol)
-		if ((n = send(sockfd, &acknowledgement, 2, 0))==-1)
-		{
-			printf("Error in sending acknowledgement");								
+		if ((sendto(sockfd,&acknowledgement,2,0,(struct sockaddr *)&clientAddress, clientAddresslength)) < 0)
+		{	
+			printf("Error in sending acknowledgement, n = %d\n",n);								
 			exit(EXIT_FAILURE);
 		}
+		else
+			printf("Acknowledgement sent\n");
 	}
 	else{
-		printf("Error receiving Datagram\n");
+		printf("Error receiving Datagram,n=%d\n",n);
 		exit(EXIT_FAILURE);
 	}
+	
 	receivedMessage[n] = '\0';
-	printf("the received string is :\n%s", receivedMessage);
+	printf("the received string is :%s\n", receivedMessage);
 }
