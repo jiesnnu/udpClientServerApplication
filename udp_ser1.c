b4b331a6a7cd35ec22ea784b10eab25f194820f4
rohitmukherjee <mukherjeerohit93@gmail.com>
2013-10-15 14:35:05 +0800

    Variables renamed following C convention

--- udp_ser1.c
+++ udp_ser1.c
@@ -4,12 +4,12 @@ udp_ser.c: the source file of the server in udp transmission
 #include "headsock.h"
 
 // transmitting and receiving function
-void transmitAndReceivePackets(int sockfd);                                                           
+void transmit_and_receive_packets(int sockfd);                                                           
 
 int main(int argc, char *argv[])
 {
 	int sockfd;
-	struct sockaddr_in myAddress;
+	struct sockaddr_in my_address;
 	
 	//create socket
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {			
@@ -18,31 +18,31 @@ int main(int argc, char *argv[])
 	}
 
 	//Initialize Socket Address
-	myAddress.sin_family = AF_INET;
-	myAddress.sin_port = htons(MYUDP_PORT);
-	myAddress.sin_addr.s_addr = INADDR_ANY;
-	bzero(&(myAddress.sin_zero), 8);
+	my_address.sin_family = AF_INET;
+	my_address.sin_port = htons(MYUDP_PORT);
+	my_address.sin_addr.s_addr = INADDR_ANY;
+	bzero(&(my_address.sin_zero), 8);
 
 	//Bind Socket
-	if (bind(sockfd, (struct sockaddr *) &myAddress, sizeof(struct sockaddr)) == -1) {           
+	if (bind(sockfd, (struct sockaddr *) &my_address, sizeof(struct sockaddr)) == -1) {           
 		printf("error in binding");
 		exit(EXIT_FAILURE);
 	}
 	printf("start receiving\n");
 	while(1) {
-		transmitAndReceivePackets(sockfd);                        
+		transmit_and_receive_packets(sockfd);                        
 	}
 	close(sockfd);
 	exit(EXIT_SUCCESS);
 }
 
-void transmitAndReceivePackets(int sockfd)
+void transmit_and_receive_packets(int sockfd)
 {
-	char receivedMessage[MAXSIZE];
-	int n = 0, clientAddresslength = 0;
-	struct sockaddr_in clientAddress;
+	char received_message[MAXSIZE];
+	int n = 0, client_address_length = 0;
+	struct sockaddr_in client_address;
 	int datagramCount = 0;	
-	clientAddresslength = sizeof (struct sockaddr_in);
+	client_address_length = sizeof (struct sockaddr_in);
 	//Initializing acknowledgement
 	struct ack_so acknowledgement;
 	acknowledgement.num = 1;
@@ -50,13 +50,13 @@ void transmitAndReceivePackets(int sockfd)
 	
 	
 	//receive the packet
-	if ((n=recvfrom(sockfd, &receivedMessage, DATALEN, 0, (struct sockaddr *)&clientAddress, &clientAddresslength)) > 0) {      
+	if ((n=recvfrom(sockfd, &received_message, DATALEN, 0, (struct sockaddr *)&client_address, &client_address_length)) > 0) {      
 		datagramCount++;
 		printf("Received Datagram %d \n",datagramCount);
-		printf("the received Datagram is :%s\n", receivedMessage);	
+		printf("the received Datagram is :%s\n", received_message);	
 
 		//Send acknowledgement on receiving the datagram (Stop & Wait Protocol)
-		if ((sendto(sockfd,&acknowledgement,2,0,(struct sockaddr *)&clientAddress, clientAddresslength)) < 0)
+		if ((sendto(sockfd,&acknowledgement,2,0,(struct sockaddr *) &client_address, client_address_length)) < 0)
 		{	
 			printf("Error in sending acknowledgement, n = %d\n",n);								
 			exit(EXIT_FAILURE);
@@ -69,6 +69,6 @@ void transmitAndReceivePackets(int sockfd)
 		exit(EXIT_FAILURE);
 	}
 	
-	receivedMessage[n] = '\0';
-	printf("the received string is :%s\n", receivedMessage);
+	received_message[n] = '\0';
+	printf("the received string is :%s\n", received_message);
 }
